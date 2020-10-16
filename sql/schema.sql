c60d22aadf77ee5999a35ff7edb3b13902448b60
Paul Siekmeier <siekmeierpaul@gmail.com>
2020-10-15 20:42:25 -0500

    initial commit

---
+++ sql/schema.sql
@@ -0,0 +1,46 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE one_star_restaurant (
+    "name" VARCHAR   ,
+    "year" INT   ,
+    "latitude" FLOAT   ,
+    "longitude" FLOAT   ,
+    "city" VARCHAR   ,
+    "region" VARCHAR   ,
+    "zipCode" VARCHAR   ,
+    "cuisine" VARCHAR   ,
+    "price" VARCHAR   ,
+    "url" VARCHAR   ,
+    PRIMARY KEY (name, region)
+);
+
+CREATE TABLE two_star_restaurant (
+    "name" VARCHAR   ,
+    "year" INT   ,
+    "latitude" FLOAT   ,
+    "longitude" FLOAT   ,
+    "city" VARCHAR   ,
+    "region" VARCHAR   ,
+    "zipCode" VARCHAR   ,
+    "cuisine" VARCHAR   ,
+    "price" VARCHAR   ,
+    "url" VARCHAR   ,
+  	PRIMARY KEY (name, region)
+);
+
+CREATE TABLE three_star_restaurant (
+    "name" PK VARCHAR   ,
+    "year" INT   ,
+    "latitude" FLOAT   ,
+    "longitude" FLOAT   ,
+    "city" VARCHAR   ,
+    "region" VARCHAR   ,
+    "zipCode" VARCHAR   ,
+    "cuisine" VARCHAR   ,
+    "price" VARCHAR   ,
+    "url" VARCHAR  ,
+	PRIMARY KEY (name, region)   
+);
+
