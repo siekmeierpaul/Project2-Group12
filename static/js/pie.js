348e49165639e5f929917c3c5a3dfbb7e4a5cb9b
Ringlis1995 <roseinglis827@gmail.com>
2020-10-20 21:13:57 -0500

    updated pie.js

--- static/js/pie.js
+++ static/js/pie.js
@@ -7,11 +7,35 @@ console.log("Loaded pie.js");
 d3.json("/restaurants").then(function (data) {
 
     // ... and dump that JSON to the console for inspection
-    console.log(data); 
+    // console.log(data); 
 
+    var cuisinecount = {};
+    
+    for (var i = 0; i < data.length; i++) { 
+        if (data[i].cuisine in cuisinecount) {
+            cuisinecount[data[i].cuisine]++;
+            // console.log(`incrementing ${data[i].cuisine}`);
+        } else {
+            cuisinecount[data[i].cuisine] = 1;
+            // console.log(`adding ${data[i].cuisine}`);
+        }
+    }
 
+    var cuisinetypes = Object.keys(cuisinecount);
+    var cuisinecounts = Object.values(cuisinecount); 
+
+    var trace1 = {
+        labels: cuisinetypes.slice(0,10),
+        values: cuisinecounts.slice(0,10),
+        type: 'pie'
+      };
+      
+
+      var data = [trace1];
+      
+      var layout = {
+        title: "Pie Chart",
+      };
 
     Plotly.newPlot("plot", data, layout);
 });
-
-
