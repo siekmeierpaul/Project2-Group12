7f034753229659ee56e045d2ab3cb8a2d4a20776
Ringlis1995 <roseinglis827@gmail.com>
2020-10-22 21:51:54 -0500

    completed pie.js with dropdowns

--- static/js/pie.js
+++ static/js/pie.js
@@ -38,4 +38,168 @@ d3.json("/restaurants").then(function (data) {
       };
 
     Plotly.newPlot("plot", data, layout);
-});
+})
+
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  var data = [];
+
+  dataset == "restaurants"
+
+  if (dataset == "one-star") {
+    d3.json("/one-star").then(function (data) {
+
+        // ... and dump that JSON to the console for inspection
+        // console.log(data); 
+    
+        var cuisinecount = {};
+        
+        for (var i = 0; i < data.length; i++) { 
+            if (data[i].cuisine in cuisinecount) {
+                cuisinecount[data[i].cuisine]++;
+                // console.log(`incrementing ${data[i].cuisine}`);
+            } else {
+                cuisinecount[data[i].cuisine] = 1;
+                // console.log(`adding ${data[i].cuisine}`);
+            }
+        }
+    
+        var cuisinetypes = Object.keys(cuisinecount);
+        var cuisinecounts = Object.values(cuisinecount); 
+    
+        var trace1 = {
+            labels: cuisinetypes.slice(0,10),
+            values: cuisinecounts.slice(0,10),
+            type: 'pie'
+          };
+          
+    
+          var data = [trace1];
+          
+          var layout = {
+            title: "Pie Chart",
+          };
+    
+        Plotly.newPlot("plot", data, layout);
+  })}
+  
+  else if (dataset == "two-star") {
+    d3.json("/two-star").then(function (data) {
+
+        // ... and dump that JSON to the console for inspection
+        // console.log(data); 
+    
+        var cuisinecount = {};
+        
+        for (var i = 0; i < data.length; i++) { 
+            if (data[i].cuisine in cuisinecount) {
+                cuisinecount[data[i].cuisine]++;
+                // console.log(`incrementing ${data[i].cuisine}`);
+            } else {
+                cuisinecount[data[i].cuisine] = 1;
+                // console.log(`adding ${data[i].cuisine}`);
+            }
+        }
+    
+        var cuisinetypes = Object.keys(cuisinecount);
+        var cuisinecounts = Object.values(cuisinecount); 
+    
+        var trace1 = {
+            labels: cuisinetypes.slice(0,10),
+            values: cuisinecounts.slice(0,10),
+            type: 'pie'
+          };
+          
+    
+          var data = [trace1];
+          
+          var layout = {
+            title: "Pie Chart",
+          };
+    
+        Plotly.newPlot("plot", data, layout);
+  })}
+
+  else if (dataset == "three-star") {
+    d3.json("/three-star").then(function (data) {
+
+        // ... and dump that JSON to the console for inspection
+        // console.log(data); 
+    
+        var cuisinecount = {};
+        
+        for (var i = 0; i < data.length; i++) { 
+            if (data[i].cuisine in cuisinecount) {
+                cuisinecount[data[i].cuisine]++;
+                // console.log(`incrementing ${data[i].cuisine}`);
+            } else {
+                cuisinecount[data[i].cuisine] = 1;
+                // console.log(`adding ${data[i].cuisine}`);
+            }
+        }
+    
+        var cuisinetypes = Object.keys(cuisinecount);
+        var cuisinecounts = Object.values(cuisinecount); 
+    
+        var trace1 = {
+            labels: cuisinetypes.slice(0,10),
+            values: cuisinecounts.slice(0,10),
+            type: 'pie'
+          };
+          
+    
+          var data = [trace1];
+          
+          var layout = {
+            title: "Pie Chart",
+          };
+    
+        Plotly.newPlot("plot", data, layout);
+  })}
+
+  else if (dataset == "restaurants") {
+    d3.json("/restaurants").then(function (data) {
+
+        // ... and dump that JSON to the console for inspection
+        // console.log(data); 
+    
+        var cuisinecount = {};
+        
+        for (var i = 0; i < data.length; i++) { 
+            if (data[i].cuisine in cuisinecount) {
+                cuisinecount[data[i].cuisine]++;
+                // console.log(`incrementing ${data[i].cuisine}`);
+            } else {
+                cuisinecount[data[i].cuisine] = 1;
+                // console.log(`adding ${data[i].cuisine}`);
+            }
+        }
+    
+        var cuisinetypes = Object.keys(cuisinecount);
+        var cuisinecounts = Object.values(cuisinecount); 
+    
+        var trace1 = {
+            labels: cuisinetypes.slice(0,10),
+            values: cuisinecounts.slice(0,10),
+            type: 'pie'
+          };
+          
+    
+          var data = [trace1];
+          
+          var layout = {
+            title: "Pie Chart",
+          };
+    
+        Plotly.newPlot("plot", data, layout);
+  })}
+  
+
+}
